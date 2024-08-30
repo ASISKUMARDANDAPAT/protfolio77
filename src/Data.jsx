3ec1619a743c730591b28dd1b45731e098cbb1b3
ASIS KUMAR DANDAPAT <asiskumardandapat90@gmail.com>
2024-08-30 14:05:41 +0530

    Initial commit

---
+++ src/Data.jsx
@@ -0,0 +1,231 @@
+import Project1 from './assets/project1.png';
+import Project2 from './assets/project2.png';
+import Project3 from './assets/project3.png';
+import Project4 from './assets/project4.jpg';
+import Project5 from './assets/project5.png';
+import Project6 from './assets/project6.png';
+
+export const links = [
+  {
+    name: 'Home',
+    path: 'home',
+  },
+  {
+    name: 'What I do',
+    path: 'projects',
+  },
+  {
+    name: 'Skills',
+    path: 'skills',
+  },
+  {
+    name: 'Projects',
+    path: 'work',
+  },
+  {
+    name: 'Resume',
+    path: 'resume',
+  },
+  {
+    name: 'Blog',
+    path: 'blog',
+  },
+  {
+    name: 'Contact',
+    path: 'contact',
+  },
+];
+
+export const services = [
+  {
+    id: 1,
+    name: 'Web Development',
+    title: 'Web Design',
+    description:
+      
+"Crafting captivating digital experiences through thoughtful design and innovative solutions. Elevating web design to seamlessly merge aesthetics with functionality in every project.",
+  },
+  {
+    id: 2,
+    name: ' Web Development ',
+    title: ' Logo Design ',
+    description:
+    "Transforming visions into distinctive brand identities through creative and impactful logo designs. Each logo is a unique visual narrative, crafted with precision to leave a lasting impression.",
+  },
+  {
+    id: 4,
+    name: ' App Development ',
+    title: ' iOS & Android ',
+    description:
+    "Building seamless and user-centric mobile applications that bring ideas to life. Leveraging cutting-edge technologies to deliver intuitive and efficient solutions for a dynamic digital experience.",
+  },
+  {
+    id: 3,
+    name: ' User Interface & Experience ',
+    title: ' UI/UX ',
+    description:
+    "Crafting intuitive and visually engaging user interfaces, while prioritizing a seamless and delightful user experience. Transforming concepts into interactive designs that captivate and enhance user interactions.",
+  },
+  {
+    id: 5,
+    name: ' Writing ',
+    title: ' Blog Writing ',
+    description:
+      
+"Exploring diverse topics with insightful and engaging blog posts. Each piece thoughtfully crafted to inform, entertain, and inspire readers on a journey of discovery and knowledge.",
+  },
+];
+
+export const skills = [
+  {
+    id: 1,
+    name: 'HTML5 & CSS3',
+    percentage: 84,
+    description:
+      'Proficient in crafting semantic and responsive web layouts using the latest HTML5 and CSS3 features. Ensuring cross-browser compatibility and adherence to modern design principles for a visually appealing user experience.',
+  },
+
+  {
+    id: 2,
+    name: 'JavaScript',
+    percentage: 70,
+    description:
+      'Skilled in JavaScript for developing interactive and dynamic web applications. Proficient in leveraging JavaScript frameworks like React.js for building modern and scalable front-end applications. Adept at solving algorithmic challenges and handling large datasets efficiently.',
+  },
+
+  {
+    id: 3,
+    name: 'SQL',
+    percentage: 75,
+    description:
+      'Proficient in SQL with expertise in complex query writing and database management.Skilled in optimizing database performance and ensuring data integrity.Adept at solving algorithmic challenges and handling large datasets efficiently.',
+  },
+
+  {
+    id: 4,
+    name: 'React Js',
+    percentage: 54,
+    description:
+      'Expertise in React.js for building modern and scalable front-end applications. Utilizing component-based architecture and state management to create robust and efficient user interfaces.',
+  },
+
+  {
+    id: 5,
+    name: 'Figma',
+    percentage: 60,
+    description:
+      'Proficient in Figma, a collaborative design tool, for creating visually stunning and interactive user interfaces, with a continuous commitment to further enhancing Figma skills for comprehensive design proficiency.',
+  },
+
+  {
+    id: 6,
+    name: 'Github & Version Control',
+    percentage: 70,
+    description:
+      'Adept in version control using Git, facilitating collaboration and tracking changes in codebase. Experienced in managing repositories on platforms like GitHub for seamless project collaboration.',
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    img: Project1,
+    category: 'Development',
+    title: 'Planţify',
+    description:  
+      "Designed and developed a vibrant e-commerce website dedicated to plants, leveraging React.js for a seamless user experience. Implemented intuitive navigation, real-time product updates, and a secure checkout system, offering plant enthusiasts a visually engaging and efficient online shopping platform.",
+  },
+  {
+    id: 2,
+    img: Project2,
+    category: 'UI UX Design',
+    title: 'Coffee-shop',
+    description:
+      'Crafted a visually captivating e-commerce website centered around plants using Figma, meticulously designing each element for an immersive user experience',
+  },
+  {
+    id: 3,
+    img: Project3,
+    category: 'Development',
+    title: 'Wheather-app',
+    description:
+      'Developed a weather application with real-time updates using React.js, providing users with accurate weather forecasts and an intuitive user interface.',
+  },
+  {
+    id: 4,
+    img: Project4,
+    category: 'Photography',
+    title: 'MidJourney',
+    description:
+      'Using Midjourney to generate aesthetic photos for ui/ux and websites.',
+  },
+  {
+    id: 5,
+    img: Project5,
+    category: 'Development',
+    title: 'Speech -to- Text Recognition',
+    description:
+      'Developed a speech-to-text application leveraging advanced natural language processing, offering users the ability to effortlessly transcribe spoken words into written text. ',
+  },
+  {
+    id: 6,
+    img: Project6,
+    category: 'UI UX Design',
+    title: 'Journal app',
+    description:
+      'Designed an intuitive journaling app UI/UX that seamlessly blends functionality with aesthetics, providing users with an elegant and personalized platform for daily reflections. The app features an easy-to-navigate interface, thoughtful typography, and interactive elements, fostering a delightful and meaningful journaling experience.',
+  },
+];
+
+export const cv = [
+  {
+    id: 1,
+    title: 'Post Graduation',
+    subtitle: 'BPUT',
+    date: '2022 - 2024',
+    description:
+      'Despite having completed my Bachelors in Chemistry, I took the decision to shift my career trajectory by undertaking a Masters in Computer Application. This allowed me to combine my analytical aptitude with a passion for technology. I am completed my MCA degree from Trident Academy of Creative Technology in Bhubaneswar, graduating in 2024. Coming from a non-technical background initially posed challenges in grasping complex computing concepts. However, our exceptional faculty took the effort to patiently strengthen our fundamentals across a variety of languages and frameworks. Through extensive hands-on coding training on C, Java, HTML/CSS/JS along with theoretical bases in Operating Systems, DBMS, Computer Networks and Architecture, I developed a well-rounded foundation. Our rigorous curriculum also prepared me on Data Structures and Algorithms for solving computational problems efficiently.My success is reflected in an excellent CGPA, a testament to my perseverance.',
+    category: 'education',
+  },
+
+  {
+    id: 2,
+    title: 'Under Graduation',
+    subtitle: 'North Orissa University',
+    date: '2019- 2022',
+    description:
+      'I completed my Bachelors in Chemistry from North odisha University, cultivating a deep interest in plant sciences. My academic performance remained consistently strong throughout the three-year program. As part of the curriculum, I also completed an insightful research project on paddy cultivation patterns.',
+    category: 'education',
+  },
+
+  {
+    id: 3,
+    title: 'State Higher Secondary Education',
+    subtitle: 'CHSE ODISHA',
+    date: '2016 - 2018',
+    description:
+      'Due to some issues I have to switch from CBSE to CHSE board in 12th grade posed early difficulties but taught me to adapt. The transition to a new state board syllabus and exam patterns shortly before the critical exams was challenging.This experience demonstrated my resolve in the face of adversity and ability to adjust well to different systems - skills that have helped me thrive in new environments.',
+    category: 'education',
+  },
+
+  {
+    id: 4,
+    title: 'Board Of Secondary Education ,Odisha',
+    subtitle: 'Dinabandhu Bidyapitha ,Bisol',
+    date: '2006 - 2016',
+    description:
+      'I completed my schooling from Dinabandhu Bidyapitha ,Bisol, which provided me with a well-rounded education. Along with a strong academic foundation across subjects, the school had a focus on co-curricular activities for holistic development. I participated extensively in may popular exhibition like Social science exhibition, National Science Congress, and many more.',
+    category: 'education',
+  },
+
+  {
+    id: 5,
+    title: 'Softwer Developement Intern',
+    subtitle: 'TETRA TRION TECHNOLOGIES PVT. LTD ,BHUBANESWAR',
+    date: '2023 - Present',
+    description:
+      'I have been working as a software developer Intern at TETRA TRION TECHNOLOGIES PVT. LTD ,BHUBANESWAR for this year. In this role, I have gained valuable experience with front-end web and Ui/Ux development working extensively with JavaScript, ReactJS, and Figma. My mentors have helped me strengthen my programming skills and learn industry best practices for building interactive user interfaces.',
+    category: 'experience',
+  }
+];
+
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import './Home.css'
+import profileImg from '../../assets/pro.png';
+import shapeOne from '../../assets/shape-1.png';
+import shapeTwo from '../../assets/shape-2.png';
+import { FaLinkedin } from "react-icons/fa";
+import { IoIosMail } from "react-icons/io";
+import { FaFigma } from "react-icons/fa6";
+import { FaBehance } from "react-icons/fa";
+import CV from '../../assets/resume_asis.pdf';
+
+const Home = () => {
+  return (
+    <section className="home" id='home'>
+        <div className="home__wrapper">
+        <div className="home__container container">
+            <p className="home__subtitle text-cs">
+                HELLO, <span className='xyz'>MY NAME IS</span>
+            </p>
+
+            <h1 className="home__title text-cs">
+                <span>ASIS KUMAR</span> DANDAPAT
+            </h1>
+
+            <p className="home__job">
+                <span className="text-cs">I Am</span> <b>Web Developer, Ui/Ux Developer</b>
+            </p>
+
+            <div className="home__img-wrapper">
+                <div className="home__banner">
+                    <img src={profileImg} alt="" className='home__profile' />
+                </div>
+
+                <p className="home__data home__data-one">
+                    <span className="text-lg">
+                        {' '} 10 <b>+</b>
+                    </span>
+                    <span className="text-sm text-cs">
+                        {' '} Projects On <b>Web Design</b>
+                    </span>
+                </p>
+
+                
+                <img src={shapeTwo} alt="" className="shape shape__2" />
+                <img src={shapeTwo} alt="" className="shape shape__3" />
+            </div>
+            <p className="home__text">
+            
+                A creative coder with a penchant for responsive web design, I present a portfolio encompassing projects across various programming languages, predominantly in JS. Emphasizing an interest in UX/UI skills, my work showcases an intuitive approach to interface design using the latest web technologies.</p>
+
+            <div className="home__socials">
+                <a href='https://linkedin.com/in/asis-kumar-dandapat-b628a32b1' className="home__social-link" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin />
+                </a>
+
+                <a href='mailto:asiskumardandapat90@gmail.com' className="home__social-link" target="_blank" rel="noopener noreferrer">
+                    <IoIosMail />
+                </a>
+
+                <a href='https://www.figma.com/@asiskumardandap' className="home__social-link" target="_blank" rel="noopener noreferrer">
+                    <FaFigma />
+                </a>
+
+                <a href='https://www.behance.net/' className="home__social-link" target="_blank" rel="noopener noreferrer">
+                    <FaBehance />
+                </a>
+            </div>
+
+            <div className="home__btns">
+                <a download = '' href={CV} className="btn text-cs">Download CV</a>
+                <a href="#skills" className="hero__link text-cs">My Skils</a>
+            </div>
+
+        </div>
+
+        <div className="section__deco deco__left">
+            <img src={shapeOne} alt="" className="shape" />
+        </div>
+        </div>
+    </section>
+  )
+}
+
+export default Home
